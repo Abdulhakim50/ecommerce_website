3989492ce5c47da35d353756d963463e07187063
abd251 <81411376+abd251@users.noreply.github.com>
2023-12-14 23:50:42 +0300

    ...

--- app/api/create-payment-intent/route.js
+++ app/api/create-payment-intent/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import prisma from '@/libs/prisma'
 
+export const dynamic = "force-dynamic"
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
