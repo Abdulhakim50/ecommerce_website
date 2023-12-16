6a23fbe5f568f4deaf889d9d69035b8e0c6ddf8b
abd251 <81411376+abd251@users.noreply.github.com>
2023-12-16 22:16:00 +0300

    ...

--- components/FooterNav.jsx
+++ components/FooterNav.jsx
@@ -31,18 +31,16 @@ const FooterNav = ({currentUser}) => {
  <p className='text-white text-sm'>Category</p>
 </div>
   
-<div className="dropdown dropdown-hover dropdown-top ">
+<div className="dropdown dropdown-top">
           <div className='flex flex-col items-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white mt-3">
+       
+<div tabIndex={0} role="button" className="">   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white mt-3">
   <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-</svg>
-
-          <p className='text-4xl'></p>
-        
+</svg></div>
         <div className=' text-sm text-white'>Account</div>
           </div>
        
-        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32">
+        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 w-20 raddd">
           {!currentUser?(
             <>
               <li ><Link href="/login">SignIn</Link></li>
