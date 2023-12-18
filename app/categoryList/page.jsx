dfa34d4ea6ba4951b16c64bff5a7fd6913eaf28a
abd251 <81411376+abd251@users.noreply.github.com>
2023-12-18 14:22:37 +0300

    ...

--- app/categoryList/page.jsx
+++ app/categoryList/page.jsx
@@ -4,18 +4,23 @@ import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>
-        {catagories.map((category)=>{
-          <div className='grid grid-cols-6  max-md:grid-cols-3 max-sm:grid-cols-2'>
-            <Image src={category.img} width={200} height={200} alt='category image'>
+ <>
+        <div class="grid max-sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mx-10 max-sm:gap-2 items-center">
 
-                <p>{category.label}</p>
-            </Image>
-           
-            
-          </div>
-        })}
+        {catagories.map((category)=>{
+          
+        
+          return <div class="category-card bg-white overflow-hidden border border-gray-300 m-2 relative  ">
+        <Image src={category.img} width={200} height={200} alt='category image' class="object-cover w-full h-full" />
+        <div class="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-center p-2">
+        <p>{category.label}</p>
+        </div>
     </div>
+         
+          
+        })}
+        </div>
+  </>
   )
 }
 
--- components/FooterNav.jsx
+++ components/FooterNav.jsx
@@ -3,28 +3,29 @@ import React from 'react'
 import ShowCartQty from './ShowCartQty'
 import Link from 'next/link'
 import Button from './Button'
+import { useRouter } from 'next/navigation'
 
 
 const FooterNav = ({currentUser}) => {
-  
+  const router =useRouter()
   
   return (
     <div className='fixed bottom-0 w-full bg-green-500 h-[60px] flex justify-evenly md:hidden  radd '>
    
 <div className='flex flex-col items-center mt-3'>
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white" onClick={router.push('/')}>
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 </svg>
  <Link href='/' className='text-white text-sm'>Home</Link>
 </div>
 
 <div className='flex flex-col text-center items-center  mt-2'>
-        <ShowCartQty className="w-7 h-7 text-white font-black " Qty=" absolute top-[-2px] right-[-0px] text-white bg-green-950 h-3 w-3 rounded-full  flex item-center justify-center text-[10px]"/>
+        <ShowCartQty className="w-7 h-7 text-white font-black " Qty=" absolute top-[-2px] right-[-0px] text-white bg-green-950 h-3 w-3 rounded-full  flex item-center justify-center text-[10px]"  onClick={router.push('/cart')} />
         <p className='text-white text-sm'>Cart</p>
         </div>
 
         <div className='flex flex-col items-center mt-3'>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white"  onClick={router.push('/categoryList')} >
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
 </svg>
 
