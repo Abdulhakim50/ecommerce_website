ab08fa2817012f455889f9853a64ad984d619e30
abd251 <81411376+abd251@users.noreply.github.com>
2024-01-04 21:13:55 +0300

    ..

--- components/MostViewed.jsx
+++ components/MostViewed.jsx
@@ -15,7 +15,7 @@ const Newarrival = ({mostViewed}) => {
     
   return (
 <div className='mt-20 text-center font-bold text-lg'>
-     <p className='max-sm:bg-green-500 max-sm:mb-3 rad max-sm:mx-2 text-2xl'>Most Viewed</p>
+     <p className='max-sm:bg-green-500 max-sm:mb-3 rad max-sm:mx-2 text-2xl text-white'>Most Viewed</p>
      <div className=' grid max-sm:grid-cols-2  lg:grid-cols-6 md:grid-cols-3 mx-10 max-sm:mx-2 max-sm:gap-2 gap-4'  >
       
     {mostViewed.map((item)=>{ 
