a84fc4c67f37a884f6b0e1343da8b6220f70308c
abd251 <81411376+abd251@users.noreply.github.com>
2024-01-04 16:55:19 +0300

    ...

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-12 md:mt-60">
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
         <div className="mb-8 lg:col-span-2">
-          <h4 className="text-3xl font-bold mb-4">ethioMarket.com</h4>
-          <p className="text-gray-400 max-w-md  text-justify">
+          <h4 className="text-3xl font-bold mb-4 max-sm:text-center">ethioMarket.com</h4>
+          <p className="text-gray-400 max-w-md  text-justify max-sm:ml-5 mr-5">
             EthioMarket.com, the best marketplace to buy and sell products and services among our customers. We passionately believe in great bargains and excellent services for buyers and sellers. Most importantly, we firmly believe in high-quality products and exceptional customer services.
           </p>
         </div>
